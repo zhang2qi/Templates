29564b5611ae30c876a212f40e752f289a472e9d
Qi Zhang <jacky780203@sina.com>
2021-12-05 20:52:25 -0500

    Initial Commit

---
+++ CIS-1202-Templates/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CIS-1202-Templates
+//
+//  Created by Qi Zhang on 12/5/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
