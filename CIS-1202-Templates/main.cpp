7ebb97377fe7f22edee3320e021ba7dc8e4badf3
Qi Zhang <jacky780203@sina.com>
2021-12-05 21:35:02 -0500

    Initial Commit

--- CIS-1202-Templates/main.cpp
+++ CIS-1202-Templates/main.cpp
@@ -4,11 +4,68 @@
 //
 //  Created by Qi Zhang on 12/5/21.
 //
+// Qi Zhang
+// CIS 1202 101
+// Dec 5, 2021
+//
+// Write templated functions to perform math on varying numeric data types.
 
 #include <iostream>
+#include <cmath>
+#include <typeinfo>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+template <typename T>
+void half(T &full, T &half)
+{
+    string type = typeid(full).name();
+    if (type.compare("i") != 0)
+        half = full / 2.0;
+    else
+        half = round(full / 2.0);
+}
+
+template <typename T>
+T half(T &full)
+{
+    string type = typeid(full).name();
+    if (type.compare("i") != 0)
+        return full / 2.0;
+    else
+        return round(full / 2.0);
+}
+
+int main()
+{
+    double a = 7.0;
+    float b = 5.0f;
+    int c = 3;
+    
+    double a_half;
+    float b_half;
+    int c_half;
+    
+    
+    //void,  solution 1
+    
+    cout << "Solution A: " << endl << endl;
+    
+    half(a, a_half);
+    half(b, b_half);
+    half(c, c_half);
+    
+    cout << a_half << endl;
+    cout << b_half << endl;
+    cout << c_half << endl;
+    
+    //return T solution 2
+    
+    cout << endl << "Solution B: " << endl << endl;
+    cout << half(a) << endl;
+    cout << half(b) << endl;
+    cout << half(c) << endl;
+    
     return 0;
 }
